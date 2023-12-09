060363416c77670d386ae0964fc2ccc9814e7e3d
Yefta Steven Marcellius <yeftasm@gmail.com>
2023-12-09 16:22:28 +0700

    table order_item

---
+++ database/migrations/2023_12_09_091147_create_order_item_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_item', function (Blueprint $table) {
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->char('item_id', 16);
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_item');
+    }
+};
